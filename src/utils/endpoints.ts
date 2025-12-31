f06337812370b4ab967abc0eaf8ba4806c54a2da
abdulwahab <abdulwahabmahmoud99@gmail.com>
2026-01-01 00:04:28 +0200

    refactor: update login endpoint for consistency

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,6 +1,6 @@
 export const endpoints = {
   auth: {
-    login: '/auth/email-login',
+    login: '/login',
     forgetPassword: '/auth/forget-password-by-email',
     verifyForgetPasswordOtp: '/auth/verify-forget-password-otp',
     resetPasswordByEmail: '/auth/reset-password-by-email',
